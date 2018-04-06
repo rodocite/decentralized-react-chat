89b3554ed003638b309c6c26a656de1c7d0c4c89
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-04-06 15:50:26 -0700

    More styling and layout

--- src/App.js
+++ src/App.js
@@ -3,21 +3,39 @@ import styled from 'styled-components'
 import { connect } from 'react-redux'
 
 const Container = styled.div`
-  margin: 100px auto;
-  width: 400px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  box-sizing: border-box;
+  border-radius: 10px;
+  border: 1px solid black;
 `
 const InputContainer = styled.section`
+  box-sizing: border-box;
   width: 100%;
+`
+const ChatBorder = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 50px;
+  border-top: 1px solid black;
+`
+
+const ChatInput = styled.input`
+  padding: 10px;
+  font-size: 18px;
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  outline: none;
+  border: none;
+  border-radius: 10px;
+  ::placeholder {
+    font-size: 15px;
+  }
 `
 
 const Chatbox = styled.div`
+  box-sizing: border-box;
   height: 300px;
   width: 100%;
-  border: 1px solid;
   overflow-y: auto;
   word-wrap: break-word;
 `
@@ -27,11 +45,9 @@ const Message = styled.div`
 `
 
 const SubscriptionSection = styled.section`
-  padding: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  padding: 15px;
+  border-bottom: 1px solid black;
+  text-align: center;
 `
 
 class App extends Component {
@@ -52,16 +68,26 @@ class App extends Component {
     this.setState({ symKey })
   }
 
-  postMessage() {
+  postMessage(e) {
+    if (e.key !== 'Enter') return
     const { postMessage, publicKey } = this.props
     const symKey = this.state.symKey || this.props.symKey
     postMessage(this.state.message, symKey, publicKey)
+    this.setState({ message: '' })
   }
 
   render() {
     const { symKeyID, message } = this.props
     return (
       <Container>
+        <SubscriptionSection>
+          <div>{ this.state.symKey || symKeyID }</div>
+          <InputContainer>
+            <input onChange={(e) => this.setState({ symKeyInput: e.target.value })} />
+            <button onClick={(e) => this.subscribe(this.state.symKeyInput)}>Change Room</button>
+          </InputContainer>
+        </SubscriptionSection>
+
         <Chatbox>
           { message && message.map((m, index) => {
               return <Message key={index}>{m}</Message>
@@ -70,28 +96,26 @@ class App extends Component {
         </Chatbox>
 
         <InputContainer>
-          <input onChange={(e) => this.setState({ message: e.target.value })} value={ this.state.message }></input>
-          <button onClick={() => this.postMessage()}>Send</button>
+          <ChatBorder>
+            <ChatInput
+              placeholder="Write a message..."
+              onChange={(e) => this.setState({ message: e.target.value })}
+              onKeyPress={ (e) => this.postMessage(e) }
+              value={ this.state.message }
+            />
+          </ChatBorder>
         </InputContainer>
-
-        <InputContainer>
-          <input onChange={(e) => this.setState({ symKeyInput: e.target.value })} />
-          <button onClick={(e) => this.subscribe(this.state.symKeyInput)}>Change Room</button>
-        </InputContainer>
-
-        <SubscriptionSection>
-          <div>Subscription Key</div>
-          <div>{ this.state.symKey || symKeyID }</div>
-        </SubscriptionSection>
       </Container>
     )
   }
 }
 
 const mapStateToProps = (state) => {
-  const { message } = state
+  const { message, signature } = state
+  console.log(signature)
   return {
-    message
+    message,
+    signature
   }
 }
 
