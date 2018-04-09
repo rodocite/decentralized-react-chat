36fecc6bc1283f29deb95b5bffb3298bc9697503
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-04-09 15:36:27 -0700

    Lint

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,9 @@
 export const toHex = (str) => {
   let hex = ''
-  for(var i = 0; i < str.length; i++) {
+  for(let i = 0; i < str.length; i++) {
     hex = hex + '' + str.charCodeAt(i).toString(16)
   }
+
   return '0x' + hex
 }
 
@@ -10,8 +11,8 @@ export const toAscii = (hexx) => {
   const hex = hexx.toString()
   let str = ''
 
-  for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2) {
-      str = str + String.fromCharCode(parseInt(hex.substr(i, 2), 16))
+  for (let i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2) {
+    str = str + String.fromCharCode(parseInt(hex.substr(i, 2), 16))
   }
 
   return str
